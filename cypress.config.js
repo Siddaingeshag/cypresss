e4dd09d571db393e6a6555f31c0e6c11b7904d6d
Siddalingsehag <siddalingeshag@gmail.com>
2025-09-18 11:32:14 +0530

    sec comtttt

--- cypress.config.js
+++ cypress.config.js
@@ -7,13 +7,21 @@ screenshotOnRunFailure: true, // Auto-screenshot on test failure
 screenshotsFolder: 'cypress/screenshots', // Screenshots save location
 });*/
 const { defineConfig } = require('cypress');
+
 module.exports = defineConfig({
-e2e: {
-baseUrl: 'https://opensource-demo.orangehrmlive.com',
-},
-video: true, // Record videos
-videoUploadOnPasses: true, // Don't upload videos of passing tests
-videosFolder: 'cypress/videos', // Videos save location
-viewportWidth: 1280, // Set video dimensions
-viewportHeight: 720,
+  e2e: {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',   // report output folder
+      overwrite: false,               // don't overwrite old reports
+      html: false,                    // generate JSON only first
+      json: true                      // create JSON output for merging
+    },
+  },
+  video: true, // Record videos
+  videoUploadOnPasses: true, // Don't upload videos of passing tests
+  videosFolder: 'cypress/videos', // Videos save location
+  viewportWidth: 1280,
+  viewportHeight: 720,
 });
